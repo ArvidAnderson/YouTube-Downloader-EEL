03b8fd128368de24ac23874d5c10e3f5f4678eec
Arvid A <arvid.anderson@outlook.com>
2021-03-02 17:12:48 +0100

    Lots of things changed

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 
-#YouTube-DL-EEL
+# YouTube-DL-EEL
 from pytube import YouTube
 import eel
 eel.init('web')
 
 url = []
 
+
 def get_youtube_video(text):
     try:
         yt = YouTube(text)
-        print(YouTube(text).streams[1])
         url.append(text)
         video_exists_passthrough(True)
     except:
         video_exists_passthrough(False)
     return None
 
+
 @eel.expose
 def get_youtube_video_passthrough(text):
     get_youtube_video(text)
     return None
 
+
 @eel.expose
 def video_exists_passthrough(yesno):
     eel.video_exists(yesno)
-    return None
-
-@eel.expose
-def redirect_to_download():
-    eel.start('download.html', size=(500, 450), port = 8000)
     return None
 
 @eel.expose
@@ -38,11 +35,10 @@ def setThumbnailandTitle():
     print(title_text)
     eel.setThumbnailandTitle(thumbnail_url, title_text)
 
+
 @eel.expose
 def debugger():
     print("DEBUGGER RAN")
 
-
-
-
-eel.start('index.html', size=(500, 335))
+if __name__ == "__main__":
+    eel.start('index.html', size=(500, 470))
